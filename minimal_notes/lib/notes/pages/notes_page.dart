7d80035b3eaa0857c95ba3e834a1499af99c38a8
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-08-05 17:09:20 +0800

    Update notes_page.dart

--- minimal_notes/lib/notes/pages/notes_page.dart
+++ minimal_notes/lib/notes/pages/notes_page.dart
@@ -12,6 +12,44 @@ class NotesPage extends StatefulWidget {
 class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // textfield controller
+    final TextEditingController textController = TextEditingController();
+
+    // create a note
+    void createNote() {
+      showDialog(
+        context: context,
+
+        builder: (context) => AlertDialog(
+          content: TextField(
+            controller: textController,
+          ),
+        )
+      );
+    }
+
+    // read notes
+
+    // update a note
+
+    // delete a note
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+
+        title: const Text(
+          "Notes"
+          ),
+      ),
+
+      // add note button
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => createNote(),
+
+        child: const Icon(Icons.add),
+      ),
+
+    );
   }
 }
