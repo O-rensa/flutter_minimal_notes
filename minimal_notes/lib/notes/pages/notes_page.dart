152d1fcd7e250f8faa64705a706f54c157de90ed
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-08-05 18:29:02 +0800

    Update notes_page.dart

--- minimal_notes/lib/notes/pages/notes_page.dart
+++ minimal_notes/lib/notes/pages/notes_page.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:minimal_notes/shared/Models/note.dart";
+import "package:minimal_notes/shared/Models/note_database.dart";
+import "package:provider/provider.dart";
 
 class NotesPage extends StatefulWidget {
   const NotesPage({
@@ -10,38 +13,133 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+    @override
+  void initState() {
+    super.initState();
+
+    // on start up read all notes
+    readNotes();
+  }
+
+  // textfield controller
+  final TextEditingController textController = TextEditingController();
+
+  // create a note
+  void createNote() {
+    showDialog(
+      context: context,
+  
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // create button
+          MaterialButton(
+            onPressed: () {
+              // add to db
+              context.read<NoteDatabase>().createNote(textController.text);
+  
+              //clear the text controller
+              textController.clear();
+  
+              // pop the dialog
+              Navigator.pop(context);
+            },
+  
+            child: const Text("Create"),
+          )
+        ],
+      )
+    );
+  }
+  
+  // read notes
+  void readNotes() {
+    context.read<NoteDatabase>().getAllNotes();
+  }
+  
+  // update a note
+  void updateNote(Note note) {
+    // pre-fill the current note text
+    textController.text = note.text;
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: const Text("Update Note"),
+        content: TextField(controller: textController),
+        actions: [
+          // update button
+          MaterialButton(
+            onPressed: () {
+              context.read<NoteDatabase>().updateNote(note.id, textController.text);
+              // cler controller
+              textController.clear();
+              // pop dialog
+              Navigator.pop(context);
+            },
+
+            child: const Text("Update"),
+          )
+        ],
+      ),
+    );
+  }
+  
+  // delete a note
+  void deleteNote(int id) {
+    context.read<NoteDatabase>().deleteNote(id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // textfield controller
-    final TextEditingController textController = TextEditingController();
+    // note database
+    final noteDatabase = context.watch<NoteDatabase>();
 
-    // create a note
-    void createNote() {
-      showDialog(
-        context: context,
-
-        builder: (context) => AlertDialog(
-          content: TextField(
-            controller: textController,
-          ),
-        )
-      );
-    }
-
-    // read notes
-
-    // update a note
-
-    // delete a note
+    // current notes
+    List<Note> currentNotes =  noteDatabase.currentNotes;
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        elevation: 10,
 
         title: const Text(
           "Notes"
           ),
       ),
+
+      body: ListView.builder(
+        itemCount: currentNotes.length,
+        itemBuilder: (context, index) {
+            // get individual note
+            final note = currentNotes[index];
+
+            // list tile UI
+            return ListTile(
+              title: Text(
+                note.text,
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+
+                children: [
+                  // edit button
+                  IconButton(
+                    onPressed: () => updateNote(note),
+                    icon: const Icon(Icons.edit),
+                  ),
+
+                  // delete button
+                  IconButton(
+                    onPressed: () => deleteNote(note.id),
+                    icon: const Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            );
+          }
+        ),
 
       // add note button
       floatingActionButton: FloatingActionButton(
